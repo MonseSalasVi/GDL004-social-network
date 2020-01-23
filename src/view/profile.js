ec83724ba23aaff2734636ab522780f58c2e82e8
Monse Salas <cmonse.salasv@gmail.com>
2020-01-23 09:03:58 -0600

    sign in google, sign up, css login

--- src/view/profile.js
+++ src/view/profile.js
@@ -1,34 +1,32 @@
 export default () => {
     const viewProfile = `
-    
-    <div class="container_user">
+    <h2> ¡Soy el Profile </h2>
+    <div class="container">
         <div class="profile_information">
             <span class="flaticon-051-user"> User </span> 
-        </div>  
-            <div class="profile_description"> Descripción: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices faucibus elit ac rutrum. Donec porttitor tortor aliquam leo pellentesque, pulvinar condimentum sapien condimentum. 
+            <div class="profile_description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices faucibus elit ac rutrum. Donec porttitor tortor aliquam leo pellentesque, pulvinar condimentum sapien condimentum. 
             </div>
-            <div class="profile_direction"> Dirección: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </div>   
-          <div class="profile_pesornal_information"> Phonee number</div>
-          <div class="flaticon-020-heart profile_icons"> iconos
-          </div>          
-         
+            <div class="profile_direction">  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
+             
+        </div>   
     </div>
-    <div class="container_post">
-    <div class="profile_information">
-        <span class="flaticon-051-user"> User </span> 
-    </div>  
-        <div class="profile_post"> Post: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices faucibus elit ac rutrum. Donec porttitor tortor aliquam leo pellentesque, pulvinar condimentum sapien condimentum. 
-        </div>
-        <div class="profile_post_icons"> 
-        <i class="flaticon-020-heart"></i><i class="flaticon-034-chat"></i><i class="flaticon-018-settings"></i> iconos
-        </div> 
-         
-     
-</div>`;
+    <div class="container">
+        <div class="profile_information">
+            <span class="flaticon-051-user"> User </span> 
+            <div class="profile_description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices faucibus elit ac rutrum. Donec porttitor tortor aliquam leo pellentesque, pulvinar condimentum sapien condimentum. 
+            </div>
+            <div class="profile_direction">  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
+             
+        </div>   
+        <li> <button id=log-out type="submit"> <a href="">SALIRRRRR </button> </li>
+           
+           
+    </div>`
 
-    const divElemt = document.createElement('div');
-    
-    divElemt.innerHTML = viewProfile;
-    return divElemt;
-};
+    const divElem = document.createElement('div')
+    divElem.innerHTML = viewProfile;
+
+    return divElem;
+}
